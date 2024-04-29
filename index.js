6c85661c46c78c3a8901f0e0e5dc38568d597ca0
sherazirshad680 <134085129+sherazirshad680@users.noreply.github.com>
2024-04-29 16:55:49 +0500

    Array Loop Practice

---
+++ index.js
@@ -0,0 +1,29 @@
+// var a: string[] = ["pakistan", "lahore", "karachi"]
+// console.log(a);
+// for (let i = 0; i < a.length; i++) {
+//     var b
+//     console.log(a [i]);
+// }
+// Array Destructuring
+// var cities: string[] = ["lahore", "karachi", "faisalabad"]
+// var [city1, city2, city3] = cities
+// console.log("city1", city1);
+// console.log("city3",city3);
+// var student = {
+//     name: "sheraz",
+//     rollNo: 55,
+//     age: 15,
+//     class: 8
+// }
+// var students: any = [{ name: "sherza", rollNo: 55, age: 15, class: 8 }, student]
+// console.log(student);
+// console.log(students[0].name);
+// Array with three elements
+
+
+// var myArray = [1, 2, 3];
+// // Array destructuring
+// var a = myArray[0], b = myArray[1], c = myArray[2];
+// console.log(a); // Output: 1
+// console.log(b); // Output: 2
+// console.log(c); // Output: 3
